ea80fc39d0affe798633865dbfe34109ff10b303
Olajide <clickcom007@yahoo.com>
2022-07-04 00:51:40 -0700

    100 again

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ for (number_left = 48; number_right <= 78; number_left++)
 for (number_right = number_left + 1 ; number_right <= 78; number_right++)
 {
 putchar(number_left);
-putchar (number_right);
+putchar(number_right);
 if ((number_left == 56) && (number_right == 78))
 {
 break;
