9c1250a02d80983d59bca064e5ffe7c5a0f03282
Olajide <clickcom007@yahoo.com>
2022-07-05 10:37:05 -0700

    102-fibonacci.c last sorting

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-
 /**
 * main - fibonacci 50
 * Return:  (Success)
 */
 
 int main(void)
-
 {
 unsigned long int i;
 unsigned long int bef = 1;
 unsigned long int aft = 2;
-
 printf("%lu", bef);
-
 for (i = 1; i < 50; i++)
 {
 printf(", %lu", aft);
@@ -22,3 +18,4 @@ bef = aft - bef;
 }
 printf("\n");
 return (0);
+}
