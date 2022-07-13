06e6c292b1d190e25754828cc372c1a3a7567769
Olajide <clickcom007@yahoo.com>
2022-07-13 03:53:23 -0700

    print 7 correction

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,41 +13,23 @@ int n, length, mid;
 length = 0;
 while (str[length] != '\0')
 {
-
 length++;
-
 }
-
 if (length % 2 == 0)
-
 {
 mid = length / 2;
-
 for (n = mid; n < length; n++)
 {
-
 _putchar(str[n]);
-
 }
-
 }
-
 else
-
 {
-
 mid = (length - 1) / 2;
-
 for (n = mid + 1; n < length; n++)
-
 {
-
 _putchar(str[n]);
-
 }
-
 }
-
 _putchar('\n');
-
 }
