6bacba746e67c7aa689f1284217fe7e2338e95c6
Igbalaye Olajide <clickcom007@yahoo.com>
2022-10-28 02:24:18 -0700

    dynamic lib

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,5 @@
-#ifndef _MAIN_H_
-
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,5 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif /* _MAIN_H_ */
+#endif /* MAIN_H */
